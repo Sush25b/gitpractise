b3f434044031f56408e85bd359372f23419ea9b2
Sushant Sukhadeo Phulsundar <sushantsukhadeophulsundar@Sushants-MacBook-Pro.local>
2019-09-19 11:34:41 +0530

    add new

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,7 +1,6 @@
 public class test
 {
     public static void main(String[] args) {
-
         System.out.println("Hello world sushant- conflict is resolve now (which was their bzc of remote & local diff commits done on one file)");
     }
 }
