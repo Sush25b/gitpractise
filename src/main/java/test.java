395680105ebf9f5cd400355c3d70978573aa3b86
Sushant Sukhadeo Phulsundar <sushantsukhadeophulsundar@Sushants-MacBook-Pro.local>
2019-09-19 11:49:35 +0530

    final push

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,6 +1,6 @@
 public class test
 {
     public static void main(String[] args) {
-        System.out.println("Hello world sushant- conflict is resolve now (which was their bzc of remote & local diff commits done on one file)");
+        System.out.println("Hello world sushant- merge newbranch1 with master branch");
     }
 }
